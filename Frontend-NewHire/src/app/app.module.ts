c225bced300ffabaf73a15060acd36d78eae4b10
Nalla chandana <nallachandana193@gmail.com>
2026-01-21 13:58:19 +0530

    Fix: add Frontend-NewHire as normal folder

---
+++ Frontend-NewHire/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { JobSearchComponent } from './job-search/job-search.component';
+import { PostVacancyComponent } from './post-vacancy/post-vacancy.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    JobSearchComponent,
+    PostVacancyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
