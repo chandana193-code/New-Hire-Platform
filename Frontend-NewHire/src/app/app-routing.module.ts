10ebe1ec0d56371ca71afb6575916f5c7f844756
Sakthi-B26 <sakthiboopathi26@gmail.com>
2026-01-21 17:32:16 +0530

    updated job-description

--- Frontend-NewHire/src/app/app-routing.module.ts
+++ Frontend-NewHire/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HeaderComponent } from './header/header.component';
 import { JobSearchComponent } from './job-search/job-search.component';
 import { PostVacancyComponent } from './post-vacancy/post-vacancy.component';
 import { FormsComponent } from './forms/forms.component';
+import { JobDescriptionComponent } from './job-description/job-description.component';
 
 const routes: Routes = [
   {path:'header', component: HeaderComponent},
   {path:'job-search', component: JobSearchComponent},
   {path:'post-vacancy', component: PostVacancyComponent},
   {path:'',component:FormsComponent},
+  {path:'job-description', component:JobDescriptionComponent}
 ];
 
 @NgModule({
