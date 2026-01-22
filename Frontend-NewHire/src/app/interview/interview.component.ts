2e07203bacd58070ac7bd44d1d4154a4e307c12a
webdschool <webdschool@gmail.com>
2026-01-22 11:58:32 +0530

    interview

---
+++ Frontend-NewHire/src/app/interview/interview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interview',
+  templateUrl: './interview.component.html',
+  styleUrls: ['./interview.component.css']
+})
+export class InterviewComponent {
+  interviews:any=[];
+
+}
